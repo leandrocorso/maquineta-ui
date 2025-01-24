5ee1775f91e620413b9b64500056b69f0974ce2c
Leandro Corso <contato@leandrocorso.com.br>
2025-01-24 16:58:20 -0300

    updating version

--- dist/theme.d.ts
+++ dist/theme.d.ts
@@ -1,2 +1,26 @@
+declare module "@mui/material/styles" {
+    interface Palette {
+        green: Palette["primary"];
+        yellow: Palette["primary"];
+    }
+    interface PaletteOptions {
+        green?: PaletteOptions["primary"];
+        yellow?: PaletteOptions["primary"];
+    }
+    interface PaletteColor {
+        text?: string;
+    }
+    interface SimplePaletteColorOptions {
+        text?: string;
+    }
+    interface TypographyVariants {
+        recordsetTitle: React.CSSProperties;
+        recordsetCaption: React.CSSProperties;
+    }
+    interface TypographyVariantsOptions {
+        recordsetTitle?: React.CSSProperties;
+        recordsetCaption?: React.CSSProperties;
+    }
+}
 declare const theme: import("@mui/material/styles").Theme;
 export default theme;
