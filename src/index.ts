f365b94ce936adca2ec53ba913055f831eda60f7
Leandro Corso <contato@leandrocorso.com.br>
2025-01-25 00:11:16 -0300

    fixing maquineta-logo.svg path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import theme from "./theme";
 import ThemeWrapper from "./ThemeWrapper";
+import { MaquinetaLogo } from "./components/MaquinetaLogo";
 
 export { theme, ThemeWrapper };
 export * from "./components/Icon";
+export { MaquinetaLogo };
